d85fd5ec2e7fa5359d84b74d14c260cee7cb926b
Christian C. Berclaz <christian.berclaz@mac.com>
2025-03-10 14:57:44 +0100

    feat(fuzzy_loader): Added the option to show the state date

--- plugin/resurrect/fuzzy_loader.lua
+++ plugin/resurrect/fuzzy_loader.lua
@@ -2,7 +2,9 @@ local wezterm = require("wezterm")
 local pub = {}
 
 ---@alias fmt_fun fun(label: string): string
----@alias fuzzy_load_opts {title: string, description: string, fuzzy_description: string, is_fuzzy: boolean, ignore_workspaces: boolean, ignore_tabs: boolean, ignore_windows: boolean, fmt_window: fmt_fun, fmt_workspace: fmt_fun, fmt_tab: fmt_fun }
+---@alias fuzzy_load_opts {title: string, description: string, fuzzy_description: string, is_fuzzy: boolean,
+---ignore_workspaces: boolean, ignore_tabs: boolean, ignore_windows: boolean, fmt_window: fmt_fun, fmt_workspace: fmt_fun,
+---fmt_tab: fmt_fun, fmt_date: fmt_fun, show_state_with_date: boolean, date_format: string }
 
 ---Returns default fuzzy loading options
 ---@return fuzzy_load_opts
@@ -15,6 +17,14 @@ function pub.get_default_fuzzy_load_opts()
 		ignore_workspaces = false,
 		ignore_windows = false,
 		ignore_tabs = false,
+		date_format = "%d-%m-%Y %H:%M:%S",
+		show_state_with_date = false,
+		fmt_date = function(date)
+			return wezterm.format({
+				{ Foreground = { AnsiColor = "White" } },
+				{ Text = date },
+			})
+		end,
 		fmt_workspace = function(label)
 			return wezterm.format({
 				{ Foreground = { AnsiColor = "Green" } },
@@ -57,17 +67,100 @@ function pub.fuzzy_load(window, pane, callback, opts)
 		end
 	end
 
-	local function insert_choices(type, fmt)
-		for _, file in ipairs(wezterm.glob("*", require("resurrect.state_manager").save_state_dir .. "/" .. type)) do
-			local label
-			local id = type .. "/" .. file
+	-- local function insert_choices(type, fmt)
+	-- end
 
-			if fmt then
-				label = fmt(file)
-			else
-				label = file
+	local max_length = 0
+
+	local function insert_choices(type, fmt)
+		local folder = require("resurrect.state_manager").save_state_dir
+		local fallback = function(root_folder, state_type, state_fmt)
+			for _, file in ipairs(wezterm.glob("*", root_folder .. Separator .. state_type)) do
+				local label
+				local id = type .. "/" .. file
+
+				if state_fmt then
+					label = state_fmt(file)
+				else
+					label = file
+				end
+				table.insert(state_files, { id = id, label = label })
 			end
-			table.insert(state_files, { id = id, label = label })
+		end
+
+		if opts.show_state_with_date then
+			local files = {}
+
+			-- Command-line recipe based on OS
+			local cmd
+			if Is_windows then
+				cmd = "powershell -Command \"Get-ChildItem -Path '"
+					.. folder
+					.. '\' | ForEach-Object { "$($_.LastWriteTime.ToFileTimeUtc()) $($_.Name)" }"'
+			elseif Is_mac then
+				cmd = 'stat -f "%m %N" ' .. folder .. Separator .. type .. "/*"
+			else -- last option: Linux-like
+				cmd = 'ls -l --time-style=+"%s" ' .. folder .. Separator .. type(" | awk '{print $6,$7,$9}'")
+			end
+
+			-- Execute the command and capture stdout
+			local handle = io.popen(cmd)
+			if handle == nil then
+				wezterm.emit("resurrect.error", "Could not open process: " .. cmd)
+				fallback(folder, type, fmt) -- Execute the fallback function
+				return
+			end
+
+			local stdout = handle:read("*a")
+			if stdout == nil or stdout == "" then
+				wezterm.emit("resurrect.error", "No output when running: " .. cmd)
+				fallback(folder, type, fmt) -- Execute the fallback function
+				if stdout == "" then
+					handle:close()
+				end
+				return
+			end
+
+			handle:close()
+
+			-- Parse the stdout and construct the file table
+			for line in stdout:gmatch("[^\n]+") do
+				local epoch, file = line:match("(%d+)%s+(.+)")
+				if epoch and file then
+					local filename, _ = file:match("^.*" .. Separator .. "(.+)%.(.*)$")
+					local formatted_date = os.date(opts.date_format, tonumber(epoch))
+					-- 		table.insert(state_files, { id = id, label = label })
+					max_length = math.max(max_length, #label)
+					table.insert(files, {
+						id = type .. "/" .. file,
+						filename = filename,
+						date = formatted_date,
+					})
+				end
+			end
+
+			for _, file in ipairs(files) do
+				local prefixed_date = " "
+				if #file.label < max_length then
+					prefixed_date = " " .. string.rep(".", max_length - #file.label - 1) .. file.formatted_date
+				else
+					prefixed_date = " " .. file.formatted_date
+				end
+				local label = {}
+				if fmt then
+					table.insert(label, fmt(file.filename))
+				else
+					table.insert(label, { text = file.filename })
+				end
+				if opts.fmt_date then
+					table.insert(label, opts.fmt_date(prefixed_date))
+				else
+					table.insert(label, { text = prefixed_date })
+				end
+				table.insert(state_files, { id = file.id, label = label })
+			end
+		else
+			fallback(folder, type, fmt)
 		end
 	end
 
