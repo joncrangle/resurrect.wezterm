344995ebca75b67e18f4fc5045aee12d4b074a92
Christian C. Berclaz <christian.berclaz@mac.com>
2025-03-12 12:02:16 +0100

    fix(utils): properly initialize the variables

--- plugin/resurrect/utils.lua
+++ plugin/resurrect/utils.lua
@@ -1,7 +1,9 @@
+local wezterm = require("wezterm") --[[@as Wezterm]] --- this type cast invokes the LSP module for Wezterm
+
 local utils = {}
 
-utils.is_mac = nil
-utils.is_windows = nil
-utils.separator = nil
+utils.is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
+utils.is_mac = (wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin")
+utils.separator = utils.is_windows and "\\" or "/"
 
 return utils
