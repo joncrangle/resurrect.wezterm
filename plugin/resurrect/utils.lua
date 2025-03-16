6753780f560458261fcd246d57a0a0482d7e053a
Christian C. Berclaz <christian.berclaz@mac.com>
2025-03-16 17:02:09 +0100

    Added two helper functions for fuzsy_loader

--- plugin/resurrect/utils.lua
+++ plugin/resurrect/utils.lua
@@ -6,4 +6,14 @@ utils.is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 utils.is_mac = (wezterm.target_triple == "x86_64-apple-darwin" or wezterm.target_triple == "aarch64-apple-darwin")
 utils.separator = utils.is_windows and "\\" or "/"
 
+-- Helper function to escape pattern special characters
+function utils.escape_pattern(str)
+	return str:gsub("([%(%)%.%%%+%-%*%?%[%]%^%$])", "%%%1")
+end
+
+-- Helper function to remove formatting sequence in strings
+function utils.strip_format(str)
+	return str:gsub(string.char(27) .. "%[[^m]*m")
+end
+
 return utils
